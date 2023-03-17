4725d49c3932aafd96c7907e0facfa56c485fcaa
Abdalla badr <Bossmail28@gmail.com>
2023-03-17 17:45:41 +0200

    Add files via upload

---
+++ uid_access.ino
@@ -0,0 +1,95 @@
+/*
+ * 
+ * All the resources for this project: https://www.hackster.io/Aritro
+ * Modified by Aritro Mukherjee
+ * 
+ * 
+ */
+ #include<EEPROM.h>
+#include <Servo.h> 
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
+
+void setup() 
+{
+  int address=1;
+  String object="I'd like to introduce this";
+   myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  EEPROM.put( 1, "I'd like to introduce this" );
+  myservo.write(pos);
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "62 A4 61 FC") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    
+
+//----------------------------------------
+
+
+
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(5000);    
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 90 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+//------------------------------------------
+  
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    delay(15);
+
+    
+  }
+} 
